9b85dac0e6dea3e45d5529a6c035a24d709855cb
mahmoudashry98 <83143927+mahmoudashry98@users.noreply.github.com>
2024-06-04 06:02:10 +0300

    add icons for login with the social media

--- lib/features/auth/screens/login/widget/login_footer.dart
+++ lib/features/auth/screens/login/widget/login_footer.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_app/utils/constants/image_strings.dart';
+import 'package:ecommerce_app/utils/constants/sizes.dart';
 import 'package:ecommerce_app/utils/helpers/helper_funcation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -11,29 +13,66 @@ class LoginFooter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = AppHelperFunctions.isDarkMode(context);
-    return Row(
+    return Column(
       children: [
-        Flexible(
-          child: Divider(
-            color: dark ? AppColors.darkGrey : AppColors.grey,
-            thickness: 0.5,
-            indent: 60,
-            endIndent: 5,
-          ),
+        Row(
+          children: [
+            Flexible(
+              child: Divider(
+                color: dark ? AppColors.darkGrey : AppColors.grey,
+                thickness: 0.5,
+                indent: 60,
+                endIndent: 5,
+              ),
+            ),
+            Text(
+              AppStrings.orSignInWith.capitalize!,
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
+            Flexible(
+              child: Divider(
+                color: dark ? AppColors.darkGrey : AppColors.grey,
+                thickness: 0.5,
+                indent: 5,
+                endIndent: 60,
+              ),
+            ),
+          ],
         ),
-        Text(
-          AppStrings.orSignInWith.capitalize!,
-          style: Theme.of(context).textTheme.labelLarge,
+        const SizedBox(
+          height: AppSize.spaceBtwItems,
         ),
-        Flexible(
-          child: Divider(
-            color: dark ? AppColors.darkGrey : AppColors.grey,
-            thickness: 0.5,
-            indent: 5,
-            endIndent: 60,
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildSocialMediaIcons(ImageStrings.googleIcon),
+            const SizedBox(
+              width: AppSize.spaceBtwItems,
+            ),
+            _buildSocialMediaIcons(ImageStrings.facebookIcon),
+          ],
         )
       ],
+    );
+  }
+
+  Widget _buildSocialMediaIcons(String image) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        border: Border.all(
+          strokeAlign: 1,
+          color: AppColors.grey,
+        ),
+      ),
+      child: IconButton(
+        onPressed: () {},
+        icon: Image(
+          width: AppSize.iconMd,
+          height: AppSize.iconMd,
+          image: AssetImage(image),
+        ),
+      ),
     );
   }
 }
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,7 +1,12 @@
 class ImageStrings{
 
+  ///logo
   static const String logoDark = 'assets/logos/logo-black.png';
   static const String logoLight = 'assets/logos/logo-white.png';
+
+  ///Icons
+  static const String facebookIcon = 'assets/icons/facebook-icon.png';
+  static const String googleIcon = 'assets/icons/google-icon.png';
   
   ///onBoarding
   static const String onBoarding1 = 'assets/images/sammy-line-sailor-on-mast-looking-through-telescope.gif';
